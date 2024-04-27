3df59f606073bb0259d3149cb1eef9b144b4f0f2
= <pratheekmulleria@gmail.com>
2024-04-27 12:08:06 +0530

    Templates settings added

--- BloodLevelApp/views.py
+++ BloodLevelApp/views.py
@@ -1,16 +1,8 @@
-# example/views.py
+# BloodLevelApp/views.py
 from datetime import datetime
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, 'home.html')
